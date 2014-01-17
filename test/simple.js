8545de8568c8afa41f744a96681664c0fb222fa3
Jérôme Bourgeais <bourgeais.jerome@gmail.com>
2014-01-17 12:16:06 +0100

    Changing expect to assert removing expect dependencie

--- test/simple.js
+++ test/simple.js
@@ -1,13 +1,12 @@
-var expect = require('expect.js'),
-    assert = require("assert"),
+var assert = require("assert"),
     ee     = require('events').EventEmitter;
 var l = require('../nlog').newL(__dirname);
 
 describe('event', function(){
   it('should find a foo log event', function(){
     l.addListener('log', function( _severity, _msg, _val, _func){
-        expect( _severity).to.equal('note');
-        expect( _msg).to.equal('foo');
+        assert.equal('note', _severity);
+        assert.equal('foo', _msg);
     });
     l.note('foo');
   });
